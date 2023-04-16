c5662bde69e17d936c43ba39d5855a971681a2b3
Alex Li <islicy@outlook.com>
2023-04-16 12:59:12 -0400

    Add guest page

--- src/components/HostHomePage.js
+++ src/components/HostHomePage.js
@@ -17,9 +17,149 @@ import {
 } from "@ant-design/icons";
 import Text from "antd/lib/typography/Text";
 import React from "react";
-import { getStaysByHost } from "../utils";
+import { deleteStay, getStaysByHost, getReservationsByStay } from "../utils";
+import UploadStay from "./UploadStay";
 
 const { TabPane } = Tabs;
+
+class ReservationList extends React.Component {
+  state = {
+    loading: false,
+    reservations: [],
+  };
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData = async () => {
+    this.setState({
+      loading: true,
+    });
+
+    try {
+      const resp = await getReservationsByStay(this.props.stayId);
+      this.setState({
+        reservations: resp,
+      });
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
+  render() {
+    const { loading, reservations } = this.state;
+
+    return (
+      <List
+        loading={loading}
+        dataSource={reservations}
+        renderItem={(item) => (
+          <List.Item>
+            <List.Item.Meta
+              title={<Text>Guest Name: {item.guest.username}</Text>}
+              description={
+                <>
+                  <Text>Checkin Date: {item.checkin_date}</Text>
+                  <br />
+                  <Text>Checkout Date: {item.checkout_date}</Text>
+                </>
+              }
+            />
+          </List.Item>
+        )}
+      />
+    );
+  }
+}
+
+class ViewReservationsButton extends React.Component {
+  state = {
+    modalVisible: false,
+  };
+
+  openModal = () => {
+    this.setState({
+      modalVisible: true,
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      modalVisible: false,
+    });
+  };
+
+  render() {
+    const { stay } = this.props;
+    const { modalVisible } = this.state;
+
+    const modalTitle = `Reservations of ${stay.name}`;
+
+    return (
+      <>
+        <Button onClick={this.openModal} shape="round">
+          View Reservations
+        </Button>
+        {modalVisible && (
+          <Modal
+            title={modalTitle}
+            centered={true}
+            visible={modalVisible}
+            closable={false}
+            footer={null}
+            onCancel={this.handleCancel}
+            destroyOnClose={true}
+          >
+            <ReservationList stayId={stay.id} />
+          </Modal>
+        )}
+      </>
+    );
+  }
+}
+
+class RemoveStayButton extends React.Component {
+  state = {
+    loading: false,
+  };
+
+  handleRemoveStay = async () => {
+    const { stay, onRemoveSuccess } = this.props;
+    this.setState({
+      loading: true,
+    });
+
+    try {
+      await deleteStay(stay.id);
+      onRemoveSuccess();
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <Button
+        loading={this.state.loading}
+        onClick={this.handleRemoveStay}
+        danger={true}
+        shape="round"
+        type="primary"
+      >
+        Remove Stay
+      </Button>
+    );
+  }
+}
 
 export class StayDetailInfoButton extends React.Component {
   state = {
@@ -131,8 +271,10 @@ class MyStays extends React.Component {
                   <StayDetailInfoButton stay={item} />
                 </div>
               }
-              actions={[]}
-              extra={null}
+              actions={[<ViewReservationsButton stay={item} />]}
+              extra={
+                <RemoveStayButton stay={item} onRemoveSuccess={this.loadData} />
+              }
             >
               {
                 <Carousel
@@ -164,7 +306,7 @@ class HostHomePage extends React.Component {
           <MyStays />
         </TabPane>
         <TabPane tab="Upload Stay" key="2">
-          <div>Upload Stays</div>
+          <UploadStay />
         </TabPane>
       </Tabs>
     );
